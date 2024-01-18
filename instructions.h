6c2999ec55fedd833ca5be9137a3de0f702134e5
kurves <kavssimply@gmail.com>
2024-01-18 08:44:21 -0800

    addd instructios header

--- instructions.h
+++ instructions.h
@@ -12,12 +12,8 @@
  */
 typedef struct instruction_s
 {
-    char *opcode;
-    void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-void push(stack_t **stack, int value, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
-void pint(stack_t **stack, unsigned int line_number);
-
 extern instruction_t instructions[];
 #endif
