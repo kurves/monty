515a638f33e761d4af2ad0a99c0f065c2ebbf2c4
kurves <kavssimply@gmail.com>
2024-01-18 09:01:33 -0800

    add pint functinality

--- pint.c
+++ pint.c
@@ -12,7 +12,7 @@
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
-	if (*stack == NULL)
+	if (stack == NULL || stack == NULL)
 	{
 		fprintf(stderr, "Error: L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
